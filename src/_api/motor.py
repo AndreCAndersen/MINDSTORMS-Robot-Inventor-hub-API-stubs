90ae6910d1005ab2d566bea9a60ab33006da1b63
Andre C. Andersen <andre@andersen.im>
2021-12-28 21:06:12 +0100

    Adding more docs.

--- src/_api/motor.py
+++ src/_api/motor.py
@@ -128,7 +128,23 @@ class Motor:
         if speed is not None:
             assert 0 <= speed <= 100
 
-    def get_position(self, *args, **kwargs):
+    def get_position(self) -> int:
+        """Returns the actual absolute position of the motor.
+
+        Example:
+
+            from mindstorms import MSHub, Motor
+
+            hub = MSHub()
+            motor_a = Motor('A')
+
+            motor_a.run_for_seconds(1, 100)
+            hub.light_matrix.write('Position =')
+            hub.light_matrix.write(motor_a.get_position())
+            motor_a.stop()
+
+        :return: The current motor's position.
+        """
         pass
 
     def get_speed(self) -> int:
@@ -150,22 +166,122 @@ class Motor:
         """
         pass
 
-    def get_degrees_counted(self, *args, **kwargs):
+    def get_degrees_counted(self) -> int:
+        """Returns the motor's relative position of degrees counted. You can set the degrees counted by using
+        `set_degrees_counted()`.
+
+        Example:
+
+            from mindstorms import MSHub, Motor
+
+            hub = MSHub()
+            motor_a = Motor('A')
+
+            motor_a.run_for_seconds(1, 100)
+            hub.light_matrix.write('degrees counted =')
+            hub.light_matrix.write(motor_a.get_degrees_counted())
+            motor_a.stop()
+
+        :return: The motor's degrees counted position.
+        """
         pass
 
-    def set_degrees_counted(self, *args, **kwargs):
+    def set_degrees_counted(self, degrees_counted: int):
+        """Sets the degrees counted to the desired count.
+
+        :param degrees_counted: The new degrees counted count.
+        """
         pass
 
-    def get_default_speed(self, *args, **kwargs):
-        pass
+    def get_default_speed(self) -> int:
+        """Returns the default motor speed.
 
-    def set_default_speed(self, *args, **kwargs):
-        pass
+        Example:
 
-    def set_stop_action(self, *args, **kwargs):
-        pass
+            from mindstorms import MSHub, Motor
 
-    def set_stall_detection(self, *args, **kwargs):
+            hub = MSHub()
+            motor_a = Motor('A')
+
+            motor_a.set_default_speed(50)
+            motor_a.run_for_rotations(1, 100)
+            hub.light_matrix.write('default speed =')
+            hub.light_matrix.write(motor_a.get_default_speed())
+
+        :return: The default motor's speed.
+        """
+
+    def set_default_speed(self, default_speed: int):
+        """Sets the default motor speed. This is the default speed value used by the other motor functions when
+        you do not specify a speed value. Setting the default speed does not affect the actual motor speed.
+        This function should be used before another function that runs motors.
+
+        Example:
+
+            from mindstorms import Motor
+            from mindstorms.control import wait_for_seconds
+
+            motor_a = Motor('A')
+
+
+            motor_a.set_default_speed(30)
+            # The motor start running for 2 seconds at the default speed (30%)
+            motor_a.run_for_seconds(2)
+            # The motor start running at 85 speed
+            motor_a.start(85)
+            # The default speed is changed to 20%, not the actual motor speed
+            motor_a.set_default_speed(20)
+            wait_for_seconds(2)
+            motor_a.stop()
+
+        :param default_speed: The desired default speed.
+        """
+        assert -100 <= default_speed <= 100
+
+    def set_stop_action(self, action: Optional[str] = COAST):
+        """Sets the default stop action the motor performs when it stops running. Setting the default stop action does
+        not affect the actual motor stop action. This function should be used before other functions that run or stop
+        motors.
+
+        Example:
+
+            from mindstorms import Motor
+
+            motor_a = Motor('A')
+
+            # The motor is free after it completely stops.
+            motor_a.set_stop_action('coast')
+            motor_a.run_for_seconds(1, 100)
+            # The motor keeps braking after it completely stops.
+            motor_a.set_stop_action('brake')
+            motor_a.run_for_seconds(1, 100)
+            # The motor holds its position after it completely stops.
+            motor_a.set_stop_action('hold')
+            motor_a.run_for_seconds(1, 100)
+
+        :param action: The desired stop action.
+        """
+        assert action in (self.COAST, self.BRAKE, self.HOLD)
+
+    def set_stall_detection(self, stop_when_stalled: Optional[bool] = True):
+        """Turns the stall detection on or off. Stall detection senses when a motor has been blocked and can't move.
+        If stall detection has been enabled and a motor is blocked, the motor will be powered off after two seconds and
+        the current motor command will be interrupted. If stall detection has been disabled, the motor will keep trying
+        to run and prgorams will "get stuck" until the motor is no longer blocked. Stall detection is enabled by
+        default.
+
+        Example:
+
+            from mindstorms import Motor
+
+            motor = Motor('A')
+
+            motor.set_stall_detection(False)
+            motor.run_for_rotations(2)
+            # The program will never proceed to here if the motor is stalled.
+
+        :param stop_when_stalled: Choose "True" to enable stall detection or "False" to disable it.
+        """
         pass
 
     def run_to_degrees_counted(self, degrees: int, speed: Optional[int] = None):
